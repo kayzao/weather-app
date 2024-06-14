3bc4c0a98fa4b28bc1d5ab58220db7b632da8a67
Kelvin Zhao <78127948+kayzao@users.noreply.github.com>
2024-06-14 19:24:13 -0400

    maybe works i dont know

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import Weather from './components/weather';
 
 function App() {
+  const [lat, setLat] = useState(null); //initialized as null
+  const [long, setLong] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      console.log("Latitude is:", lat);
+      console.log("Longitude is:", long);
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures this runs only once
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      if (lat && long) {
+        await fetch(`${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`)
+          .then(res => res.json())
+          .then(result => {
+            setData(result);
+            console.log(result);
+          })
+          .catch(error => {
+            console.error('Error fetching the weather data:', error);
+          });
+      } else {
+        console.log("lat and long dont exist");
+      }
+    };
+
+    fetchWeatherData();
+  }, [lat, long]); // Fetch weather data whenever lat or long change
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data ? (
+        <Weather weatherData={data}/> //adds data as a prop to weather
+      ) : (
+        <div>
+          <p>Loading...</p>
+        </div>
+      )}
     </div>
   );
 }
